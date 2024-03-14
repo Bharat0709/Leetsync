065d89b90af7a872a1ea196daa9bf6060093c38d
Bharat Pahwa <102669662+Bharat0709@users.noreply.github.com>
2024-03-14 22:30:29 +0530

    Time: 107 ms (84.22%) | Memory: 62.2 MB (60.81%) - LeetSync

---
+++ 1582-design-browser-history/design-browser-history.cpp
@@ -0,0 +1,40 @@
+struct Node {
+    string val;
+    Node* prev=NULL;
+    Node* next=NULL;
+};
+class BrowserHistory {
+public:
+Node* curr=NULL;
+    BrowserHistory(string homepage) {
+        curr=new Node();
+        curr->val=homepage;
+    }
+    
+    void visit(string url) {
+        Node* temp=new Node();
+        temp->val=url;
+        curr->next=temp;
+        temp->prev=curr;
+        curr=temp;
+    }
+    
+    string back(int steps) {
+        while(curr->prev && steps>0)
+        {
+            curr=curr->prev;
+            steps--;
+        }
+        return curr->val;
+        
+    }
+    
+    string forward(int steps) {
+       while(curr->next&& steps>0)
+       {
+           curr=curr->next;
+           steps--;
+       } 
+       return curr->val;
+    }
+};
